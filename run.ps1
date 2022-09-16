113d200b3553765fc0720a0aa679a71049f93cc6
Vincent Lin <soulace2425@outlook.com>
2022-09-15 22:47:15 -0700

    Enhance script to use $PSScriptRoot trick

--- run.ps1
+++ run.ps1
@@ -2,14 +2,14 @@
 
 $START_IN = Join-Path $home "repos\localbot"
 
-# Assert that script is run with current directory at $START_IN
-if ((Get-Location).ToString() -ne $START_IN) {
-    Write-Host "Script must be run at $START_IN, aborted." -ForegroundColor Red
+# Assert that script is run with cwd at file's location
+if ((Get-Location).ToString() -ne $PSScriptRoot) {
+    Write-Host "Script must be run from its directory, aborted." -ForegroundColor Red
     exit 1
 }
 
 # Assert that script is run with the project venv active
-if (!$env:VIRTUAL_ENV -or (Split-Path $env:VIRTUAL_ENV -Parent) -ne $START_IN) {
+if (!$env:VIRTUAL_ENV -or (Split-Path $env:VIRTUAL_ENV -Parent) -ne $PSScriptRoot) {
     Write-Host "Script must be run with the project venv active, aborted." -ForegroundColor Red
     exit 1
 }
