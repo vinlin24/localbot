a64e06ff33b9f4788d69eee0e249b04145ed9ea2
Vincent Lin <soulace2425@outlook.com>
2022-09-26 18:33:23 -0700

    Implement draft of cd autocomplete

--- bot/commands/cd.py
+++ bot/commands/cd.py
@@ -3,7 +3,10 @@
 Defines the cd command.
 """
 
+from pathlib import Path
+
 from discord import Interaction, app_commands
+from discord.app_commands import Choice
 
 from ..client import CWD, LocalBot
 from ..utils import escape_path, get_path, validate_path
@@ -25,6 +28,38 @@ async def cd_command(interaction: Interaction, path_str: str) -> None:
     await interaction.response.send_message(
         f"Changed working directory to **{escape_path(CWD.path)}**."
     )
+
+
+@cd_command.autocomplete("path_str")
+async def cd_autocomplete(interaction: Interaction,
+                          current: str
+                          ) -> list[Choice[str]]:
+    # Pretend the current string is a path
+    # The autocomplete options then are its "siblings"
+
+    # TODO: Doesn't work as expected for partial inputs with trailing slash
+    # e.g. repos/counters/ doesn't bring up children of the counters dir
+    # This also causes . and .. to not work as expected
+
+    path = Path(current)
+    if path.is_absolute():
+        siblings = path.parent.iterdir() if current else path.iterdir()
+    else:
+        parent = (CWD.path / path).parent if current else CWD.path
+        siblings = parent.iterdir()
+
+    result = []
+    for sibling in siblings:
+        if len(result) == 25:
+            break
+        if sibling.is_dir() and path.name in sibling.name:
+            full_path = sibling.resolve()
+            print(full_path)
+            result.append(Choice(
+                name=str(full_path),
+                value=str(full_path)
+            ))
+    return result
 
 
 async def setup(bot: LocalBot) -> None:
